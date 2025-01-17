f0bc6d8ba6b015b46f80ddb8baf386a951de862c
Wellyton <wellytonsdj@gmail.com>
2025-01-16 21:17:12 -0300

    resolvi a 5

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
 // Imprimir(SOMA);
 // Ao final do processamento, qual será o valor da variável SOMA?
-console.log('#1 - SOMATOTAL');
+console.log('\n#1 - SOMATOTAL');
 (function somaTotal() {
   let indice = 13
   let soma = 0
@@ -17,7 +17,7 @@ console.log('#1 - SOMATOTAL');
 })()
 
 // 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
-console.log(`#2 - FIBONNACCI \n`)
+console.log(`\n#2 - FIBONNACCI \n`)
 let entrada = 34;
 (function checkFibonacci() {
   let first = 0;
@@ -51,7 +51,7 @@ let entrada = 34;
 // a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 // b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
-console.log(`#3 - FATURAMENTOS \n`)
+console.log(`\n#3 - FATURAMENTOS \n`)
 const faturamento = require('./faturamento.json')
 // eliminando os dias de finais de semana e feriados (item.valor === 0)
 const faturamentos = faturamento.filter(item => item.valor !== 0 && item.diaSemana !== 'sabado' && item.diaSemana !== 'domingo');
@@ -97,7 +97,7 @@ const faturamentos = faturamento.filter(item => item.valor !== 0 && item.diaSema
 // Calcula o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
 
 
-console.log('#4 - FATURAMENTO MENSAL \n')
+console.log('\n#4 - FATURAMENTO MENSAL \n')
 const faturamentoMensal = [
   { estado: 'SP', valor: 'R$67.836,43' },
   { estado: 'RJ', valor: 'R$36.678,66' },
@@ -124,4 +124,22 @@ function calcularPercentual(faturamento) {
 }
 
 calcularPercentual(faturamentoMensal);
+
+// 5) Escreva um programa que inverta os caracteres de um string.
+
+// IMPORTANTE:
+// a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+// b) Evite usar funções prontas, como, por exemplo, reverse;
+
+console.log(`\n#5 Inverter String \n`)
+const inverterString = str => {
+  // Percorre a string de trás pra frente e soma cada caracter em uma variável, invertendo a ordem dos caracteres.
+  let resultado = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    resultado += str[i];
+  }
+  return resultado;
+}
+
+console.log(inverterString('Olá, mundo!'));
 
